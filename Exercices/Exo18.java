d8d12ba6fee55687d3ad292782d6ecd3232584ac
Thony <anthony.rita83@gmail.com>
2025-12-07 16:11:42 +0100

    f

---
+++ Exercices/Exo18.java
@@ -0,0 +1,12 @@
+package Exercices;
+import java.util.Scanner;
+
+public class Exo18 
+{
+    public static void main(String[]args)
+    {
+        Scanner Sc = new Scanner(System.in);
+        
+        Sc.close();
+    }
+}
